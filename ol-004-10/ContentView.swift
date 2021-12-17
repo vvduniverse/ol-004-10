959e3d73dc64f4c012eb2106136d760af5c64b69
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-17 21:37:25 +0200

    Initial commit

--- ol-004-10/ContentView.swift
+++ ol-004-10/ContentView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tapCount = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Tap count \(tapCount)") {
+            self.tapCount += 1
+        }
     }
 }
 
