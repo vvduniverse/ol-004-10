f8f7c66423d49ec380c9985cb6078fcbf7c9e4b5
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-17 21:34:27 +0200

    Initial Commit

---
+++ ol-004-10/ol_004_10App.swift
@@ -0,0 +1,17 @@
+//
+//  ol_004_10App.swift
+//  ol-004-10
+//
+//  Created by Vahtee Boo on 17.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ol_004_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
